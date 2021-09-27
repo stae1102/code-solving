03be06b26501bd79c190930b34633ea5ead66c9f
sungtae <83271772+stae1102@users.noreply.github.com>
2021-09-27 18:27:35 +0900

    Update 3009 네 번째 점.py
    사각형을 구할 때, 이미 기존에 세 개의 점이 주어진다. 즉 하나의 점은 x값과 y값이 짝이 없는 값이라는 것이다.
    
    예를 들어서 (5,5), (5,7), (7,5) 라면, 7,7이 답이어야 한다.

--- baekjoon/Levels/기본수학 2단계/3009 네 번째 점.py
+++ baekjoon/Levels/기본수학 2단계/3009 네 번째 점.py
@@ -4,12 +4,13 @@ y = []
 for _ in range(3):
     a, b = map(int, input().split())
     if a in x:
-        x.remove(a)
+        x.remove(a)        # 이미 값이 있으면 제거
     else:
         x.append(a)
     if b in y:
-        y.remove(b)
+        y.remove(b)        # 이미 값이 있으면 제거
     else:
         y.append(b)
 
-print(x[0], y[0])
+print(*x, *y)
+
