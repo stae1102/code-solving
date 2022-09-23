528fdda2ce8229f5af9e8afb7438d2eb8fbadf27
stae1102 <stae1102@gmail.com>
2022-09-23 09:25:12 +0900

    Baekjoon 2011 solved

--- baekjoon/2011 암호코드.py
+++ baekjoon/2011 암호코드.py
@@ -2,16 +2,27 @@ import sys
 sys.setrecursionlimit(10**6)
 div = 1000000
 
-
-def find(x):
-
-
 arr = sys.stdin.readline().rstrip()
-l = len(arr)
-d = [0] * l
+l = len(arr) - 1
+d = [0] * (l + 1)
 
-if arr[0] == "0" or "00" in arr:
-    print(0)
-else:
-    print(find(0))
-print(d)
+
+def encrypt(i):
+    if i > l:
+        return 1
+
+    if d[i] != 0:
+        return d[i]
+
+    if arr[i] != "0":
+        d[i] += encrypt(i + 1) % div
+        if i + 1 <= l:
+            if int(arr[i:i + 2]) <= 26:
+                d[i] += encrypt(i + 2) % div
+    else:
+        return 0
+
+    return d[i] % div
+
+
+print(encrypt(0))
