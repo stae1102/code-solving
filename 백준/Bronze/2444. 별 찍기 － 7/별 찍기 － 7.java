4279991b6f1c4b52b34bdde45a4d2fb2be064e11
Seongtae <stae1102@gmail.com>
2023-12-05 20:04:16 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -7,30 +7,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                sb.append(" ");
             }
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print("*");
+            for (int j = 0; j < 2 * i - 1; j++) {
+                sb.append("*");
             }
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            sb.append("\n");
         }
 
-        for (int i = 0; i < n - 1 ; i++) {
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print(" ");
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                sb.append(" ");
             }
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print("*");
+            for (int j = 0; j < 2 * i - 1; j++) {
+                sb.append("*");
             }
-            for (int j = 0; j < n - i - 2; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
