3f3e1363b909f05030810bb2bccdcd2945151347
Seongtae <stae1102@gmail.com>
2023-12-11 21:12:53 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86156 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int value = 666;
+        int count = 0;
+
+        while(true) {
+            if (String.valueOf(value).contains("666")) {
+                count += 1;
+            }
+
+            if (count == n) {
+                System.out.println(value);
+                break;
+            }
+
+            value++;
+        }
+    }
+}
